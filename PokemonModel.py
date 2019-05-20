c891b1b7f9aa93dcb6ee8e88714dc4e05fccd997
bohdan.lushchyk <bohdan.lushchyk@devcom.com>
2019-05-20 13:20:30 +0300

    renamed

--- PokemonModel.py
+++ PokemonModel.py
@@ -90,26 +90,3 @@ class PokemonFetch:
             data_stats = await PokemonFetch.fetch(session, PokemonFetch.url_pok_stats.format(id))
             data_varaities = await PokemonFetch.fetch(session, PokemonFetch.url_pok_varaites.format(id))
             return Pokemon(data_stats , data_varaities)
-
-async def main():
-    start_time = time.time()
-    pok = await PokemonFetch.get_pokemon_id(6)
-    print(pok.ToString())
-    pok_forms = await pok.GetForms()
-    for new_pokemon in pok_forms:
-        print(new_pokemon.ToString())
-
-    print("Времени понадобилось для первого вызова {}".format(time.time() - start_time))
-
-    start_time = time.time()
-    pok = await PokemonFetch.get_pokemon_id(6)
-    print(pok.ToString())
-    pok_forms = await pok.GetForms()
-    for new_pokemon in pok_forms:
-        print(new_pokemon.ToString())
-
-    print("Времени понадобилось для второго вызова {}".format(time.time() - start_time))
-
-if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
